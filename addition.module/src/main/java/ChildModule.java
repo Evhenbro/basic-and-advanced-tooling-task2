45dd6d5a27e489b5273f8504e5bba188572f92d5
Yevhen Yarema <evhenbro@gmail.com>
2019-02-14 23:27:17 +0200

    adding project on github

---
+++ addition.module/src/main/java/ChildModule.java
@@ -0,0 +1,6 @@
+public class ChildModule {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from child module)");
+    }
+}
---
+++ src/main/java/ua/com/yarema/Main.java
@@ -0,0 +1,10 @@
+package ua.com.yarema;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+}
